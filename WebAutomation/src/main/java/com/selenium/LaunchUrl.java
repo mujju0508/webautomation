556d8a2affa33dc56e67f7ab0a461be1c41aba72
mujju0508@gmail.com <mujju0508@gmail.com>
2021-05-30 18:41:50 +0530

    added new line in LaunchUrl class

--- WebAutomation/src/main/java/com/selenium/LaunchUrl.java
+++ WebAutomation/src/main/java/com/selenium/LaunchUrl.java
@@ -12,6 +12,7 @@ System.setProperty("webdriver.chrome.driver", "C:\\EclipseWorkingDirectory\\sele
 WebDriver driver = new ChromeDriver();
 driver.get("https://www.browserstack.com");
 System.out.println("URL launch browser stack");
+System.out.println("added new line");
 driver.close();
 System.out.println("URL closed");
 	}
